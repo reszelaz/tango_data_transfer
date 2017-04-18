fdac23ad869316fb3413aaae71a2afd2aec5748d
zreszela <zreszela@cells.es>
2017-04-18 16:49:53 +0200

    Use argparse in test script

--- test.py
+++ test.py
@@ -1,50 +1,56 @@
 import sys
 import threading
+import argparse
+
 import PyTango
 
 GROUP = "test/group/1"
 CONSUMER = "test/consumer/1"
 
-state = PyTango.DevState.ON
+class Test(object):
+    
+    def __init__(self):
+        self.state = PyTango.DevState.ON
 
-def push_event(event):
-    global state
-    if event.err:
-        print event.errors
-        return
-    value = event.attr_value.value
-    if (value == PyTango.DevState.ON and
-        state == PyTango.DevState.MOVING):
-        state_event.set()
-    state = value
-
-try:
-    strategy = sys.argv[1]
-except IndexError:
-    strategy = "read"
-else:
-    try:
-        repeat = int(sys.argv[2])
-    except IndexError:
-        repeat = 1
+    def push_event(self, event):
+        if event.err:
+            print event.errors
+            return
+        value = event.attr_value.value
+        if (value == PyTango.DevState.ON and
+            self.state == PyTango.DevState.MOVING):
+            self.state_event.set()
+        self.state = value
         
-group = PyTango.DeviceProxy(GROUP)
-consumer = PyTango.DeviceProxy(CONSUMER)
+    def run(self, strategy, repeat):        
+        group = PyTango.DeviceProxy(GROUP)
+        consumer = PyTango.DeviceProxy(CONSUMER)
 
-group.write_attribute("strategy", strategy)
-consumer.write_attribute("strategy", strategy)
+        group.write_attribute("strategy", strategy)
+        consumer.write_attribute("strategy", strategy)
+        
+        for _ in xrange(repeat):
+            id_ = consumer.subscribe_event("state",
+                                           PyTango.EventType.CHANGE_EVENT,
+                                           self.push_event)
+            self.state = PyTango.DevState.ON
+            self.state_event = threading.Event()
+            consumer.Start()
+            while True:
+                if self.state_event.wait(0.01):
+                    break
+            consumer.unsubscribe_event(id_)
+            if strategy == "event":
+                assert consumer.read_attribute("event_order_ok").value == True
+            print "data_time_sum ", consumer.read_attribute("data_time_sum").value
 
-for _ in xrange(repeat):
-    id_ = consumer.subscribe_event("state",
-                                   PyTango.EventType.CHANGE_EVENT,
-                                   push_event)
-    state = PyTango.DevState.ON
-    state_event = threading.Event()
-    consumer.Start()
-    while True:
-        if state_event.wait(0.01):
-            break
-    consumer.unsubscribe_event(id_)
-    if strategy == "event":
-        assert consumer.read_attribute("event_order_ok").value == True
-    print "data_time_sum ", consumer.read_attribute("data_time_sum").value
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("strategy", type=str)
+    parser.add_argument("-r", "--repeat", type=int, default=1)
+    args = parser.parse_args()
+    strategy = args.strategy
+    repeat = args.repeat
+    
+    test = Test()
+    test.run(strategy, repeat) 
